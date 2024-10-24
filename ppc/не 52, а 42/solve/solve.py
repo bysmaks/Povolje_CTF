b380e3c7d0db7e7a0cd3c8c7304007c57cc8f8a0
d1temnd <74726976+d1temnd@users.noreply.github.com>
2024-10-24 15:36:41 +0700

    update ppc zip

--- ppc/не 52, а 42/solve/solve.py
+++ ppc/не 52, а 42/solve/solve.py
@@ -1,16 +1,15 @@
 import os
-import zipfile
-
+import pyminizip
 
 def encode():
-    for i in range(1, 2345+1):
-        zip_file_name = f'task{2345+1 - i}.zip'
-        with zipfile.ZipFile(zip_file_name) as zip:
-            zip.extractall()
+    for i in range(1, 2345 + 1):
+        zip_file_name = f'task{2345 + 1 - i}.zip'
+        # Распаковка архива
+        pyminizip.uncompress(zip_file_name, None, None, 0)
 
         if i < 2345:
+            # Удаление архива после распаковки
             os.remove(zip_file_name)
-
 
 if __name__ == '__main__':
     encode()

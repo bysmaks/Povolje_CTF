61dddfc38083c26a6b36255cf770588f16710fb4
zavrik965 <69624159+zavrik965@users.noreply.github.com>
2024-11-16 10:04:29 +0300

    Fix deploy bugs

---
+++ deploy/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo -n "[STEP 1] Enter the ip/domen for openvpn in format - 'udp://domen.com': "
+read domen_openvpn
+bash -c "docker compose run --rm openvpn ovpn_genconfig -u $domen_openvpn"
+echo -n "[STEP 1] At the next steps of program type password with length more then 4. On the other question you make press only Enter"
+read
+bash -c "docker compose run --rm openvpn ovpn_initpki"
+mv config/openvpn.conf config/openvpn.conf.bak
+sed -e '/persist-tun/a \
+duplicate-cn' \
+-e '/route 192.168.254.0 255.255.255.0/a \
+route 10.0.0.0 255.255.0.0' \
+-e '/push "comp-lzo no"/a \
+push "route 10.0.0.0 255.255.0.0"' \
+config/openvpn.conf.bak > config/openvpn.conf
+bash -c "docker compose run --rm openvpn easyrsa build-client-full test nopass"
+bash -c "docker compose run --rm openvpn ovpn_getclient test > certificate.ovpn"
+bash -c "openssl req -new -x509 -key asterisk-config/keys/asterisk.key -out asterisk-config/keys/asterisk.crt -days 365"
+python3 changer.py
+if [ $? -eq 0 ]
+then
+  bash -c "cd pcapfix && make && ./pcapfix -d ../../public/release.pcapng -o ../../public/fix_release.pcapng"
+  if [ -f ../public/fix_release.pcapng ]; then
+      rm ../public/release.pcapng
+      mv ../public/fix_release.pcapng ../public/release.pcapng
+  fi
+  bash -c "docker compose up -d"
+fi
+
